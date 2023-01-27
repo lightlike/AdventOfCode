bdce1ab0a814f954d7d6652a4b2e0f18c4e5bd2f
lightlike <dnietfeld@outlook.com>
2023-01-27 13:47:53 +0100

    fix day 6

--- 2022/days/day06.py
+++ 2022/days/day06.py
@@ -5,7 +5,7 @@ from . import helper
 def find_4_different(file: Iterator[str]) -> int:
     for line in file:
         for i in range(3, len(line)):
-            chars: str = line[i-3:i+1]
+            chars: str = line[i-4:i]
             unique: set[str] = set(chars)
             if len(unique) == 4:
                 return i
@@ -13,7 +13,7 @@ def find_4_different(file: Iterator[str]) -> int:
 def find_14_different(file: Iterator[str]) -> int:
     for line in file:
         for i in range(13, len(line)):
-            chars: str = line[i-13:i+1]
+            chars: str = line[i-14:i]
             unique: set[str] = set(chars)
             if len(unique) == 14:
                 return i
@@ -26,7 +26,7 @@ def run():
 
     result: int = find_4_different(file)
 
-    print(f"Block End: {result + 1}")
+    print(f"Block End: {result}")
 
     # Round 2
     print("Round 2:")
@@ -35,4 +35,4 @@ def run():
 
     result: int = find_14_different(file)
 
-    print(f"Message Start: {result + 1}")
+    print(f"Message Start: {result}")
